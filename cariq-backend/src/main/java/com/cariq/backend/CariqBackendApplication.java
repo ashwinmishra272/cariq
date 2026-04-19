5b3c72c724de1ed273cb355612b9dacb5d11c490
ashwinmishra <ashwinmishra272@gmail.com>
2026-04-19 12:28:21 +0530

    Project initiated

---
+++ cariq-backend/src/main/java/com/cariq/backend/CariqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cariq.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CariqBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CariqBackendApplication.class, args);
+    }
+
+}
